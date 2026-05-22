7fdeb526760f3b0219e96aa6351fe4ebe96cc6bf
Nina Margaretha <tlive4444@gmail.com>
2026-05-22 16:04:32 +0800

    Update weatherboo.ps1 to run on Chrome with port 9000

---
+++ weatherboo.ps1
@@ -0,0 +1,5 @@
+# Weatherboo PowerShell Script
+# Run this script to launch Weatherboo
+
+Write-Host "Weatherboo by Ninskie is starting..." -ForegroundColor Green
+flutter run -d chrome --web-port 9000
