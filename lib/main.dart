145de4797e325f5570e40e7a7bde2495dc7b85ac
Nina Margaretha <tlive4444@gmail.com>
2026-05-22 13:08:46 +0800

    Add main.dart with routing and Supabase initialization

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'screens/splash_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/signup_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/profile_screen.dart';
+import 'providers/user_provider.dart';
+import 'utils/constants.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Supabase.initialize(
+    url: AppConstants.supabaseUrl,
+    anonKey: AppConstants.supabaseAnonKey,
+  );
+  
+  runApp(const WeatherbooApp());
+}
+
+class WeatherbooApp extends StatelessWidget {
+  const WeatherbooApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => UserProvider(),
+      child: MaterialApp(
+        title: 'Weatherboo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color(0xFF667EEA),
+            brightness: Brightness.light,
+          ),
+          useMaterial3: true,
+          scaffoldBackgroundColor: Colors.black,
+          textTheme: GoogleFonts.quicksandTextTheme(
+            const TextTheme(
+              displayLarge: TextStyle(fontWeight: FontWeight.w700),
+              displayMedium: TextStyle(fontWeight: FontWeight.w600),
+              bodyLarge: TextStyle(fontWeight: FontWeight.w400),
+              bodyMedium: TextStyle(fontWeight: FontWeight.w500),
+            ),
+          ),
+        ),
+        initialRoute: '/splash',
+        routes: {
+          '/splash': (context) => const SplashScreen(),
+          '/login': (context) => const LoginScreen(),
+          '/signup': (context) => const SignupScreen(),
+          '/home': (context) => const HomeScreen(),
+          '/profile': (context) => const ProfileScreen(),
+        },
+      ),
+    );
+  }
+}
